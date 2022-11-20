74b317a42452ff85967889c8ed0b4aa0aeba32ba
Jeff <cyjeffb@yahoo.com>
2022-11-20 16:32:06 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -21,44 +21,76 @@ public class Main {
         System.out.println("Your final score was " + highScore);*/
 
 
+        //Challenge
+        //Create a method called displayHighScorePosition
+        //it should a players name as parameter, and a 2nd parameter as a position in the high score table
+        //You should display the players name along with a message like "managed to get into position "and the
+        //position they got and a further message "on the high score table"
+
+        //Create a 2nd method called calculateHighScorePosition
+        //it should be sent one argument only, the player score
+        //it should return an int
+        //1 if the score is >1000
+        //2 if the score is >500 and < 1000
+
+        String playerName = "Jeff";
+        int playerScore = 1500;
+
+        int playerPosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Jeff", playerPosition);
+
+        playerPosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Jing", playerPosition);
+
+        playerPosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Kyle", playerPosition);
+
+        playerPosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Liam", playerPosition);
+
+        playerPosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Kirk", playerPosition);
+
+        playerPosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Anne", playerPosition);
+
+        playerPosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Edward", playerPosition);
+
+        playerPosition = calculateHighScorePosition(999);
+        displayHighScorePosition("999", playerPosition);
+
     }
-            //Challenge
-            //Create a method called displayHighScorePosition
-            //it should a players name as parameter, and a 2nd parameter as a position in the high score table
-            //You should display the players name along with a message like "managed to get into position "and the
-            //position they got and a further message "on the high score table"
 
-            //Create a 2nd method called calculateHighScorePosition
-            //it should be sent one argument only, the player score
-            //it should return an int
-            //1 if the score is >1000
-            //2 if the score is >500 and < 1000
 
-    public static void displayHighScorePosition(String playerName, int position ){
-        System.out.println("Player Name: " + playerName);
-        System.out.println("Position: " + position);
+    public static void displayHighScorePosition(String playerName, int playerHighScorePosition ){
+        System.out.println(playerName + " managed to get into position "
+                                      + playerHighScorePosition + " on the high score table");
     }
 
     public  static int calculateHighScorePosition(int score){
         int position = 0;
-        if (score > 1000){
+        if (score >= 1000){
             position = 1;
-        } else if (score > 500 && score < 1000){
+        } else if (score >= 500){
             position = 2;
-        }
+        } else if(score >= 100){
+            position = 3;
+        } else
+            position = 4;
+
         return  position;
     }
 
         //public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
-        public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+        /*public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
             int finalScore = -1;
             if (gameOver){
                 finalScore = score + (levelCompleted * bonus);
                 finalScore +=1000;
-            /*System.out.println("Game Over");
-            System.out.println("Your final score was " + finalScore);*/
+            *//*System.out.println("Game Over");
+            System.out.println("Your final score was " + finalScore);*//*
             }
             return finalScore;
-        }
+        }*/
     }
-}
